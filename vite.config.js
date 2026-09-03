e0c67a958ac04f75fc58fda56bf88d98d031eaac
JayaKumar200 <vjayakumar850@gmail.com>
2026-09-03 18:08:10 +0530

    ignore issue fixed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      ignored: ['**/dist/**', '**/.git/**', '**/node_modules/**']
+    },
     proxy: {
       '/api': {
         target: 'https://api.codingboss.in/herbal',
